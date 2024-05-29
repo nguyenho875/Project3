0aaeb79f2176c8533449baaaad9983148f13a9b0
avillacampafiuba <avillacampa@fi.uba.ar>
2024-05-29 21:51:14 +0000

    Saco comentarios del main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 //=====[Libraries]=============================================================
 
-//#include "smart_home_system.h"
 #include "system.h"
 
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
-    //smartHomeSystemInit();
     system_init();
     while (true) {
-        //smartHomeSystemUpdate();
         system_update();
     }
 }
